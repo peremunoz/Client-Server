325953f8369964b4f55f7a79942aca227573cab2
peremunoz <73690199+peremunoz@users.noreply.github.com>
2022-03-16 23:53:56 +0100

    Done some things... Details in desc
    Done InfoMsg function.
    Implemented first part of client register process.
    Received (REG_REQ) and sent (REG_REJ or REG_ACK) correctly.
    Implemented some aux functions

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 import servermodule
 import threading
 import socket
+import random
 
 
 class Colors:
@@ -29,7 +30,7 @@ class ServerCfg:
 class Client:
     Id: str
     Status: str = servermodule.DISCONNECTED
-    Id_Comm: int = 0
+    Id_Comm: str = 0
     IP_Address: str = 0
 
 
@@ -50,6 +51,10 @@ clients = []
 
 def errorMsg(text):
     print(Colors.FAIL + "[ERROR] =>\t" + text + Colors.ENDC)
+
+
+def infoMsg(text):
+    print("[INFO] =>\t" + Colors.UNDERLINE + Colors.HEADER + text + Colors.ENDC)
 
 
 def checkCfgFile(filename):
@@ -135,12 +140,69 @@ def startServer():
 def handleUDPConnections(mainUDPSocket: socket.socket):  # HANDLING UDP CONNECTIONS AND THREAD EVERY NEW CONNECTION.
     while 1:
         (bytesReceived, (ip, port)) = mainUDPSocket.recvfrom(sys.getsizeof(UDP_PDU), socket.MSG_WAITALL)
-        clientUDPThread = threading.Thread(target=handleUDPConnection, args=(bytesReceived, ip, port,))
+        clientUDPThread = threading.Thread(target=handleUDPConnection, args=(bytesReceived, ip, port, mainUDPSocket,))
         clientUDPThread.start()
 
 
-def handleUDPConnection(bytesReceived, ip, port):
+def handleUDPConnection(bytesReceived, ip, port, mainUDPSocket):
     packet = unpackUDP(bytesReceived)
+
+    if packet.Type != servermodule.REG_REQ:
+        sendREG_REJ(mainUDPSocket, ip, port, "Wrong packet type for register petition: " + packet.Type)
+
+    if not packetFromAuthedUser(packet):
+        sendREG_REJ(mainUDPSocket, ip, port, "Client with id: " + packet.Id_Trans + " not authed in server")
+
+    if packet.Id_Comm != "0000000000" or packet.Data != "":
+        sendREG_REJ(mainUDPSocket, ip, port, "Wrong information in packet REG_REQ")
+
+    clientIndex = searchClient(packet.Id_Trans)
+    if clients[clientIndex].Status != servermodule.DISCONNECTED:
+        sendREG_REJ(mainUDPSocket, ip, port, "Client with id: " + packet.Id_Trans + " it's not in status DISCONNECTED")
+
+    Id_Comm = random.randint(1000000000, 9999999999)
+    clientUDPSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    clientUDPSocket.bind(("", 0))
+    newUDPPort = clientUDPSocket.getsockname()[1]
+    sendREG_ACK(mainUDPSocket, ip, port, Id_Comm, newUDPPort)
+    clients[clientIndex].Status = servermodule.WAIT_INFO
+    infoMsg("Client with id:" + str(clients[clientIndex].Id) + " in status WAIT_INFO")
+
+
+def sendREG_ACK(socketToSend, ip, port, randomId_Comm, UDPPort):
+    REG_ACKPacket = UDP_PDU(servermodule.REG_ACK, serverCfg.Id, randomId_Comm, UDPPort)
+    REG_ACKPacked = packUDP(REG_ACKPacket)
+    bytesSent = socketToSend.sendto(REG_ACKPacked, (ip, port))
+    while bytesSent != 84:      # 84 = size of UDP_PDU
+        bytesSent += socketToSend.sendto(REG_ACKPacked[bytesSent:], (ip, port))
+
+
+def searchClient(clientId):
+    for i in range(sys.getsizeof(clients)):
+        if clients[i].Id == clientId:
+            return i
+
+
+def packetFromAuthedUser(packet: UDP_PDU):
+    userToSearch = packet.Id_Trans
+    for i in range(sys.getsizeof(clients)):
+        if clients[i].Id == userToSearch:
+            return True
+    return False
+
+
+def sendREG_REJ(socketToSend, ip, port, reason):
+    REG_REJPacket = UDP_PDU(servermodule.REG_REJ, serverCfg.Id, "0000000000", reason)
+    REG_REJPacked = packUDP(REG_REJPacket)
+    bytesSent = socketToSend.sendto(REG_REJPacked, (ip, port))
+    while bytesSent != 84:
+        bytesSent += socketToSend.sendto(REG_REJPacked[bytesSent:], (ip, port))
+    exit(0)
+
+
+def packUDP(packet: UDP_PDU):
+    packedPacket = struct.pack("B 11s 11s 61s", packet.Type, str(packet.Id_Trans).encode(), str(packet.Id_Comm).encode(), str(packet.Data).encode())
+    return packedPacket
 
 
 def unpackUDP(bytesReceived: bytes):
@@ -155,8 +217,7 @@ def unpackUDP(bytesReceived: bytes):
     return UDP_PDU(packetType, packetId_Trans, packetId_Comm, packetData)
 
 
-def handleTCPConnections(
-        mainTCPSocket):  # HANDLING TCP CONNECTIONS AND THREADING EVERY NEW CONNECTIONS AND SAVING IT TO CLIENT CLASS
+def handleTCPConnections(mainTCPSocket):  # HANDLING TCP CONNECTIONS AND THREADING EVERY NEW CONNECTIONS AND SAVING IT TO CLIENT CLASS
     pass
 
 
